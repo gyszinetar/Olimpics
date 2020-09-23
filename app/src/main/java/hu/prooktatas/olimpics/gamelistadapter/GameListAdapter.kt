f553e090feef6bf8f75a1eb15c545c6ab8079892
Malasits Géza <malageza@gmail.com>
2020-09-23 18:15:01 +0200

    GameListaActivity first commit

---
+++ app/src/main/java/hu/prooktatas/olimpics/gamelistadapter/GameListAdapter.kt
@@ -0,0 +1,38 @@
+package hu.prooktatas.olimpics.gamelistadapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import hu.prooktatas.olimpics.R
+import hu.prooktatas.olimpics.TAG
+import hu.prooktatas.olimpics.model.GameInfo
+
+class GameListAdapter(val gameInfos: List<GameInfo>): RecyclerView.Adapter<GameInfoViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameInfoViewHolder {
+        val rootView = LayoutInflater.from(parent.context).inflate(R.layout.gameinfo_item_row, parent, false)
+        return GameInfoViewHolder(rootView)
+
+    }
+
+    override fun getItemCount(): Int {
+        Log.d(TAG, gameInfos.size.toString() )
+        return gameInfos.size
+    }
+
+    override fun onBindViewHolder(holder: GameInfoViewHolder, position: Int) {
+        val gi = gameInfos[position]
+        holder.textViewCity.text = gi.city
+        holder.textViewCountry.text = gi.country
+        holder.textViewYear.text = gi.year.toString()
+    }
+
+}
+
+class GameInfoViewHolder(v: View): RecyclerView.ViewHolder(v) {
+    val textViewCountry = v.findViewById<TextView>(R.id.textViewCountry)
+    val textViewCity = v.findViewById<TextView>(R.id.textViewCity)
+    val textViewYear = v.findViewById<TextView>(R.id.textViewYear)
+}
