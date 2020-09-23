e38890c18e4ac0f975d2c847af89f11efac1e947
gyszinetar <gyszinetar@gmail.com>
2020-09-23 18:22:04 +0200

    Create models from datas

--- app/src/main/java/hu/prooktatas/olimpics/persistence/dao/GameDao.kt
+++ app/src/main/java/hu/prooktatas/olimpics/persistence/dao/GameDao.kt
@@ -8,11 +8,19 @@ import hu.prooktatas.olimpics.persistence.entity.Game
 
 @Dao
 interface GameDao{
+    @Query("select * from games where city_id=:id")
+    fun fetchAllGameByCityId(id:Int): List<Game>
+
+    @Query("select city_id from games order by city_id desc limit 1")
+    fun fetchHighestCityId(): Int
+
 
 
     @Query("select * from games")
     fun fetchAllGame(): List<Game>
 
+    @Query("select year from games where city_id=:id")
+    fun getYearsForCity(id:Int): List<Int>
 
     @Insert
     fun insertAlbum(game: Game)
--- app/src/main/java/hu/prooktatas/olimpics/persistence/repository/OlimpicGamesRepository.kt
+++ app/src/main/java/hu/prooktatas/olimpics/persistence/repository/OlimpicGamesRepository.kt
@@ -1,7 +1,9 @@
 package hu.prooktatas.olimpics.persistence.repository
 
 import android.content.Context
+import com.google.android.gms.maps.model.LatLng
 import hu.prooktatas.olimpics.model.GameInfo
+import hu.prooktatas.olimpics.model.MarkerInfo
 import hu.prooktatas.olimpics.persistence.OlimpicsDatabase
 
 class OlimpicGamesRepository(var context: Context) {
@@ -19,5 +21,38 @@ class OlimpicGamesRepository(var context: Context) {
             list.add(GameInfo(country.name,city.name,it.year))
         }
         return list
+    }
+
+    fun buildMarkerInfo():List<MarkerInfo>{
+        val daoGame = OlimpicsDatabase.getDatabase(context)?.gameDao()
+        val daoCity = OlimpicsDatabase.getDatabase(context)?.cityDao()
+        val daoCountry = OlimpicsDatabase.getDatabase(context)?.countryDao()
+        var allGames=daoGame!!.fetchAllGame()
+        var allCity=daoCity!!.fetchAllCity()
+        var allCountry=daoCountry!!.fetchAllCountry()
+        var list= mutableListOf<MarkerInfo>()
+
+        allCity.forEach {   city ->
+            var years=daoGame!!.getYearsForCity(city.id)
+            var countries = allCountry.filter {
+                it.id==city.country_id
+            }
+
+
+
+            var sstring : String =city.name.plus(" Country:").plus(countries[0].name).plus(" years:")
+            years.forEach{
+                sstring=sstring.plus(it.toString()).plus(", ")
+            }
+
+            list.add(MarkerInfo(LatLng(city.latitude,city.longitude),sstring))
+
+
+
+        }
+
+
+        return list
+
     }
 }
