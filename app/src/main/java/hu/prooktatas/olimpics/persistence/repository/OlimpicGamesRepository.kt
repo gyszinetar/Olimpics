a6707dbebe03b6c8cb7bd4df2bd281d8626d9f42
gyszinetar <gyszinetar@gmail.com>
2020-09-28 18:24:45 +0200

    javitas jointToString re

--- app/src/main/java/hu/prooktatas/olimpics/persistence/repository/OlimpicGamesRepository.kt
+++ app/src/main/java/hu/prooktatas/olimpics/persistence/repository/OlimpicGamesRepository.kt
@@ -10,11 +10,11 @@ import hu.prooktatas.olimpics.persistence.entity.City
 
 class OlimpicGamesRepository(var context: Context) {
     private val database = OlimpicsDatabase.getDatabase(context)
-
+    private val daoGame = OlimpicsDatabase.getDatabase(context)?.gameDao()
+    private val daoCity = OlimpicsDatabase.getDatabase(context)?.cityDao()
+    private val daoCountry = OlimpicsDatabase.getDatabase(context)?.countryDao()
     fun buildGames():List<GameInfo>{
-        val daoGame = OlimpicsDatabase.getDatabase(context)?.gameDao()
-        val daoCity = OlimpicsDatabase.getDatabase(context)?.cityDao()
-        val daoCountry = OlimpicsDatabase.getDatabase(context)?.countryDao()
+
         var list= mutableListOf<GameInfo>()
         var year = daoGame!!.fetchAllGame()
         year.forEach {
@@ -26,39 +26,25 @@ class OlimpicGamesRepository(var context: Context) {
     }
 
     fun buildMarkerInfo():List<MarkerInfo>{
-        val daoGame = OlimpicsDatabase.getDatabase(context)?.gameDao()
-        val daoCity = OlimpicsDatabase.getDatabase(context)?.cityDao()
-        val daoCountry = OlimpicsDatabase.getDatabase(context)?.countryDao()
+
         var allCity=daoCity!!.fetchAllCity()
         var allCountry=daoCountry!!.fetchAllCountry()
         var list= mutableListOf<MarkerInfo>()
-        val sstring = StringBuilder()
+
+
+
+
         allCity.forEach {   city ->
                 var years=daoGame!!.getYearsForCity(city.id)
-                var countries = allCountry.filter {
-                    it.id==city.country_id
-                }
-
-
-            sstring.append("City: ")
-            sstring.append(city.name)
-            sstring.append(" Country:")
-            sstring.append(countries[0].name)
-            sstring.append(" Year(s):")
-
-                years.forEach{
-                    sstring.append(it.toString())
-                    sstring.append(", ")
-                }
-
-                list.add(MarkerInfo(LatLng(city.latitude,city.longitude),sstring.toString().dropLast(2)))
+                val yearslist=years.joinToString(", ")
+                val cityname=city.name
+                list.add(MarkerInfo(LatLng(city.latitude,city.longitude),cityname+" "+yearslist))
         }
         return list
     }
 
     fun cityCloseToLocation(pos1:LatLng): Pair<String,String>?{
-        val daoCity = OlimpicsDatabase.getDatabase(context)?.cityDao()
-        val daoCountry = OlimpicsDatabase.getDatabase(context)?.countryDao()
+
         var allCity=daoCity!!.fetchAllCity()
         allCity.forEach {
             var array= floatArrayOf(0f)
